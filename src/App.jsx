546cebbe35ceedb2766858f608fe68f92c5e525c
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2022-01-14 09:52:19 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Basket from './pages/Basket'
+import Categories from './pages/Categories'
+import CategoryProducts from './pages/CategoryProducts'
+import NotFound from './pages/NotFound'
+import ProductDetails from './pages/ProductDetails'
+import Products from './pages/Products'
+
+function App () {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route index element={<Navigate replace to='/products' />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/categories/:id' element={<CategoryProducts />} />
+          <Route path='/basket' element={<Basket />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </main>
+    </>
+  )
+}
+
+export default App
